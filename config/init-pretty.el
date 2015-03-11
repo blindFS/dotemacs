f7546d1ed15862502738246c7d4d70c642978825
farseer90718 <farseer90718@gmail.com>
2015-03-11 13:15:52 +0800

    coq unicode

--- config/init-pretty.el
+++ config/init-pretty.el
@@ -9,26 +9,10 @@
              ("lambda" . ?λ)
              ) prettify-symbols-alist)))
 
-(defun coq-prettify-symbols-hook ()
-  (setq-local
-   prettify-symbols-alist
-   (append '(("forall" ?∀)
-             ("->" ?→)
-             ("exists" ?∃)
-             ("=>" ?⇒)
-             ("False" ?⊥)
-             ("True" ?⊤)
-             ("fun" ?λ)
-             ("/\\" ?∧)
-             ("\\/" ?∨)
-             ("nil" ?∅)) prettify-symbols-alist)))
-
 (add-hook 'scheme-mode-hook 'minor-prettify-symbols-hook)
 (add-hook 'emacs-lisp-mode-hook 'minor-prettify-symbols-hook)
 (add-hook 'racket-mode-hook 'minor-prettify-symbols-hook)
 (add-hook 'python-mode-hook 'minor-prettify-symbols-hook)
-(add-hook 'coq-mode-hook 'coq-prettify-symbols-hook)
-(add-hook 'coq-goals-mode-hook 'coq-prettify-symbols-hook)
-(add-hook 'coq-response-mode-hook 'coq-prettify-symbols-hook)
+(add-hook 'coq-mode-hook 'proof-unicode-tokens-enable)
 
 (provide 'init-pretty)
